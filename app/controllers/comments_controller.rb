cb03225858e9dd429b6142c5875657f071ac1ad8
jmuldvp <jmul.dvp@gmail.com>
2016-07-30 00:09:45 +0000

    Assignment 42 completed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,28 +3,40 @@ class CommentsController < ApplicationController
     before_action :authorize_user, only: [:destroy]
     
     def destroy
-        @post = Post.find(params[:post_id])
-        comment = @post.comments.find(params[:id])
+        # @post = Post.find(params[:post_id])
+        # comment = @post.comments.find(params[:id])
+        comment = Comment.find(params[:id])
+        
         if comment.destroy
             flash[:notice] = "Comment was deleted successfully."
-            redirect_to [@post.topic, @post]
+            redirect_to :back
         else
             flash[:alert] = "Comment couldn't be deleted. Try again."
-            redirect_to [@post.topic, @post]
+            redirect_to :back
         end
     end
     
     def create
-        @post = Post.find(params[:post_id])
-        comment = @post.comments.new(comment_params)
+        if params[:post_id]
+            @parent = Post.find(params[:post_id])
+        elsif params[:topic_id]
+            @parent = Topic.find(params[:topic_id])
+        end
+            
+        comment = @parent.comments.new(comment_params)
         comment.user = current_user
         
         if comment.save
-            flash[:notice] = "Comment saved successfully."
-            redirect_to [@post.topic, @post]
-        else
-            flash[:alert] = "Comment failed to save."
-            redirect_to [@post.topic, @post]
+            if @parent.is_a?(Post)
+                flash[:notice] = "Comment saved successfully."
+                redirect_to [@parent.topic, @parent]
+            elsif @parent.is_a?(Topic)
+                flash[:notice] = "Comment saved successfully."
+                redirect_to @parent
+            else
+                flash[:alert] = "Comment failed to save."
+                redirect_to [@parent.topic, @parent]
+            end
         end
     end
     
