8c02ea03437c2974d722087c9e0066455fbadff2
jmuldvp <jmul.dvp@gmail.com>
2016-08-20 00:36:40 +0000

    Checkpoint 47 completed part 2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,12 +5,11 @@ class CommentsController < ApplicationController
     def destroy
         @post = Post.find(params[:post_id])
         @comment = @post.comments.find(params[:id])
+        
         if @comment.destroy
             flash[:notice] = "Comment was deleted successfully."
-            # redirect_to [@post.topic, @post]
         else
             flash[:alert] = "Comment couldn't be deleted. Try again."
-            # redirect_to [@post.topic, @post]
         end
         
         respond_to do |format|
@@ -27,10 +26,8 @@ class CommentsController < ApplicationController
         
         if @comment.save
             flash[:notice] = "Comment saved successfully."
-            # redirect_to [@post.topic, @post]
         else
             flash[:alert] = "Comment failed to save."
-            # redirect_to [@post.topic, @post]
         end
         
         respond_to do |format|
