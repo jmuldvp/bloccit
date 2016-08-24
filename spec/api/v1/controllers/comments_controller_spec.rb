d2b7c54aa14bb67aeaeec21608a080ef6063662d
jmuldvp <jmul.dvp@gmail.com>
2016-08-24 03:47:19 +0000

    Assignment 48 completed 1

--- spec/api/v1/controllers/comments_controller_spec.rb
+++ spec/api/v1/controllers/comments_controller_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe Api::V1::TopicsController, type: :controller do
-    let(:my_user) { create(:user) }
+RSpec.describe Api::V1::CommentsController, type: :controller do
     let(:my_topic) { create(:topic) }
+    let(:my_user) { create(:user) }
+    # let(:other_user) { create(:user) }
     let(:my_post) { create(:post, topic: my_topic, user: my_user) }
+    let(:my_comment) { Comment.create!(body: 'Comment Body', post: my_post, user: my_user) }
     
     context "unauthenticated user" do
         it "GET index returns http success" do
@@ -12,14 +14,15 @@ RSpec.describe Api::V1::TopicsController, type: :controller do
         end
         
         it "GET show returns http success" do
-            get :show, id: my_topic.id
+            get :show, post_id: my_post.id, id: my_comment.id
             expect(response).to have_http_status(:success)
         end
         
-        it "GET show returns child posts" do
-            get :show, id: my_topic.id
+        it "GET show returns a comment" do
+            get :show, post_id: my_post.id, id: my_comment.id
             response_hash = JSON.parse response.body
-            expect(response_hash['posts']).to_not be_nil
+            expect(response_hash['id']).to eq(my_comment.id)
+            expect(response_hash['body']).to eq(my_comment.body)
         end
     end
     
@@ -34,14 +37,15 @@ RSpec.describe Api::V1::TopicsController, type: :controller do
         end
         
         it "GET show returns http success" do
-            get :show, id: my_topic.id
+            get :show, post_id: my_post.id, id: my_comment.id
             expect(response).to have_http_status(:success)
         end
         
-        it "GET show returns child posts" do
-            get :show, id: my_topic.id
+        it "GET show returns a comment" do
+            get :show, post_id: my_post.id, id: my_comment.id
             response_hash = JSON.parse response.body
-            expect(response_hash['posts']).to_not be_nil
+            expect(response_hash['id']).to eq(my_comment.id)
+            expect(response_hash['body']).to eq(my_comment.body)
         end
     end
 end
--- spec/api/v1/controllers/posts_controller_spec.rb
+++ spec/api/v1/controllers/posts_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Api::V1::TopicsController, type: :controller do
+RSpec.describe Api::V1::PostsController, type: :controller do
     let(:my_user) { create(:user) }
     let(:my_topic) { create(:topic) }
     let(:my_post) { create(:post, topic: my_topic, user: my_user) }
@@ -12,14 +12,14 @@ RSpec.describe Api::V1::TopicsController, type: :controller do
         end
         
         it "GET show returns http success" do
-            get :show, id: my_topic.id
+            get :show, topic_id: my_topic.id, id: my_post.id
             expect(response).to have_http_status(:success)
         end
         
-        it "GET show returns child posts" do
-            get :show, id: my_topic.id
+        it "GET show returns child comments" do
+            get :show, topic_id: my_topic.id, id: my_post.id
             response_hash = JSON.parse response.body
-            expect(response_hash['posts']).to_not be_nil
+            expect(response_hash['comments']).to_not be_nil
         end
     end
     
@@ -34,14 +34,14 @@ RSpec.describe Api::V1::TopicsController, type: :controller do
         end
         
         it "GET show returns http success" do
-            get :show, id: my_topic.id
+            get :show, topic_id: my_topic.id, id: my_post.id
             expect(response).to have_http_status(:success)
         end
         
-        it "GET show returns child posts" do
-            get :show, id: my_topic.id
+        it "GET show returns child comments" do
+            get :show, topic_id: my_topic.id, id: my_post.id
             response_hash = JSON.parse response.body
-            expect(response_hash['posts']).to_not be_nil
+            expect(response_hash['comments']).to_not be_nil
         end
     end
 end
